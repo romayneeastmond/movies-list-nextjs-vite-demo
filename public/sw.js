469047c360dbf1640a098d9c6555a1125f7aae21
Romayne Eastmond <romayne@gmail.com>
2026-06-08 22:52:17 -0600

    feat: PWA.

---
+++ public/sw.js
@@ -0,0 +1,34 @@
+const CACHE = 'watchlist-v1';
+const PRECACHE = ['/', '/index.html', '/favicon.svg', '/manifest.webmanifest'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(PRECACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      const network = fetch(event.request).then(response => {
+        if (response.ok) {
+          const clone = response.clone();
+          caches.open(CACHE).then(cache => cache.put(event.request, clone));
+        }
+        return response;
+      });
+      return cached || network;
+    })
+  );
+});
